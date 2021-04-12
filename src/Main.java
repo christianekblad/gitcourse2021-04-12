3587814791e664e795e2fcf37713ed1ead4fbc03
christianekblad <christian.ekblad.mailbox@gmail.com>
2021-04-12 11:21:09 +0200

    created the solution for the exercise. added the out folder to the .gitignore file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int num = 5; // set the integer "num" to 5.
+        System.out.println("before increment: " +num); // print the number.
+
+        int incrementedNumber = increment(num); // call the "increment" method and send in the number (5). then assign the result to the "incrementedNumber" integer
+        System.out.println("after increment: " +incrementedNumber); // print the incremented number.
+    }
+
+    // method to increment the number that's sent in.
+    public static int increment(int number) {
+        return ++number; // increment the number (from 5 to 6) and return it.
     }
 }
