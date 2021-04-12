d6ca9102adf7bd0ed9b28e742dd63eebac10d395
christianekblad <christian.ekblad.mailbox@gmail.com>
2021-04-12 13:15:19 +0200

    added the multiply method

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,12 @@ public class Main {
 
         int decrementedNumber = decrement(incrementedNumber); // call the "decrement" method and send in the incremented number (5). then assign the result to the "decrementedNumber" integer.
         System.out.println("after decrement: " +decrementedNumber); // print the decremented number.
+
+        int num1 = 10; // set the integer "num1" to 10.
+        int num2 = 50; // set the integer "num2" to 50.
+
+        int multipliedNumber = multiply(num1, num2);
+        System.out.println("\n" +num1 +" multiplied with " +num2 +" = " +multipliedNumber);
     }
 
     // method to increment the number that's sent in.
@@ -19,5 +25,10 @@ public class Main {
     // method to decrement the number that's sent in.
     public static int decrement(int number) {
         return --number; // decrement the number (from 6 to 5) and return it.
+    }
+
+    // method to multiply the numbers that are sent in.
+    public static int multiply(int number1, int number2) {
+        return number1*number2; // multiply the numbers (10 and 50) and return it.
     }
 }
