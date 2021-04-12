b57aca8ae71b60639e621a7027d0978c9204edf3
christianekblad <christian.ekblad.mailbox@gmail.com>
2021-04-12 14:59:15 +0200

    added a header and a line

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("GIT Exercise solutions");
+        System.out.println("----------------------");
         int num = 5; // set the integer "num" to 5.
         System.out.println("before increment: " +num); // print the number.
 
